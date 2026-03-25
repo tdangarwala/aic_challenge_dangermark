3f75b7b998d49779eee7114c81d39b19c9e58c11
Tapan Dangarwala <tapan@ip-172-31-8-100.us-east-2.compute.internal>
2026-03-25 03:10:12 +0000

    created policy node, need to test

---
+++ aic_model/act_policy_node_dm/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'act_policy_node_dm'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    package_data={'': ['py.typed']},
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='tapan',
+    maintainer_email='tapan@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
